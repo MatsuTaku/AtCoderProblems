bf1b5ae255889887ff13c6ec1805bec0920bc949
MatsuTaku <tkm.matsumoto@gmail.com>
2020-08-31 23:20:45 +0900

    Set recomendation classification ranges as idea2

--- atcoder-problems-frontend/src/pages/UserPage/Recommendation/Recommendation2.tsx
+++ atcoder-problems-frontend/src/pages/UserPage/Recommendation/Recommendation2.tsx
@@ -44,12 +44,12 @@ import {
 const logit = (x: number) => Math.log(x / (1 - x));
 const sigmoid = (x: number) => 1 / (1 + Math.exp(-x));
 
-const SolveProbabilityInf = 0.03;
+const SolveProbabilityInf = 0.05;
 const XAtSup = logit(SolveProbabilityInf);
 const SolveProbabilityThresholds = [
-  sigmoid(-XAtSup / 5),
-  sigmoid(XAtSup / 5),
-  sigmoid((XAtSup * 3) / 5),
+  sigmoid(-XAtSup / 2),
+  0.5,
+  sigmoid(XAtSup / 2),
   SolveProbabilityInf,
 ] as const;
 
@@ -143,8 +143,8 @@ export const Recommendation2: React.FC<Props> = (props) => {
 
   const recommendationDataList = [
     {
-      name: "Moderate",
-      color: theme.difficultyBlueColor,
+      name: "Stable",
+      color: theme.difficultyCyanColor,
       problems: classifiedProblemsOfId(0),
     },
     {
